d5c55d2a16c001d4edc2f05befacaef2aff91fa3
yky <jratul@nate.com>
2018-06-05 11:18:42 +0900

    fix popup bug

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var isPopupOn = false;
+var isPopupOn = true;
 
 $(function() {
 	var wheelScroll="noPop";
@@ -60,12 +60,16 @@ $(function() {
 
 	// //////////////////////////////////////////////////////////////////////
 	// 팝업
+	$(".all_cover").click(function(){
+  		$(this).addClass("noshow");
+  		isPopupOn = false;
+  	})
 	// 닫기버튼
 	$(".pop_close").click(function(){
 		isPopupOn = false;
 		$(".pop1,.pop2,.pop3,.pop4,.pop5")
 		.animate({top:"-1000px"}, 300,function(){
-			$(".pop1,.pop2,.pop3,.pop4,.pop5,.pop01_wrapper1,.pop01_wrapper2").addClass("noshow");
+			$(".pop1,.pop2,.pop3,.pop4,.pop5,.pop01_wrapper1,.pop01_wrapper2,.pop02_wrapper2,.pop02_wrapper3,.pop03_wrapper2,.pop03_wrapper3,.pop04_wrapper2,.pop04_wrapper3,.pop05_wrapper2,.pop05_wrapper3").addClass("noshow");
 		});
 		$(".pop_bg").fadeOut(500).addClass("noshow");
 		return;
